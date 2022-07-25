11519f459d991e71c68c0efd88d4cfaad0cc9c36
tamimiqbalfysal <tamimiqbal.fysal@gmail.com>
2022-07-25 15:53:56 +0600

    30. @if works

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,10 +8,14 @@
 
 @if($post['is_new'])
 <div> A new blog post! Using if </div>
+@else
+    <div> Blog post is old! using elseif/else  </div>
+
 @endif
 
-<h1> {{ $post['title'] }} </h1>
-<p> {{ $post['content'] }}</p>
+    <h1> {{$post ['title'] }} </h1>
+    <p> {{$post ['content'] }} </p>
+
 
 @endsection
 
